170496e8f65168cf90152b66e2612ed3b7cb44ee
Peter Studnicka <52224390+spam-elgoog@users.noreply.github.com>
2023-05-06 12:13:48 -0400

    Memoized file loading

--- lib/file_loader.rb
+++ lib/file_loader.rb
@@ -5,13 +5,19 @@ class FileLoader
   attr_reader :file_name, :parser
 
   def initialize(file_name, parser = JSON)
+    unless File.exist?(file_name)
+      raise "File not found: #{file_name}"
+    end
     @file_name = file_name
     @parser = parser
   end
 
   def load_data
-    json_data = File.read(file_name)
-    parser.parse(json_data)
+    unless defined? @data
+      json_data = File.read(file_name)
+      @data = parser.parse(json_data)
+    end
+    @data
   end
 
   private :file_name, :parser
