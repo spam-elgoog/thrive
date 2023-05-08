11ac25d8468095ebcf13e9781eb2ca95e168a17a
Peter Studnicka <52224390+spam-elgoog@users.noreply.github.com>
2023-05-08 00:56:54 -0400

    Add transaction_generator and tests

---
+++ lib/transaction_generator.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# Crates a hash of transactions that represents all the active users
+# that whose balance of tokens will be updated. Transactions are grouped by
+# the company id. Transactions are unsorted.
+# {
+#   3: [
+#     <DTO::Transaction:0x00000001d5a5c5a0 @company_id=3, @user_id=4, ....>,
+#     <DTO::Transaction:0x00000001d5a5c5a0 @company_id=3, @user_id=2, ....>
+#   ],
+#   1: [
+#     <DTO::Transaction:0x00000001d5a5c5a0 @company_id=1, @user_id=1, ....>,
+#     <DTO::Transaction:0x00000001d5a5c5a0 @company_id=1, @user_id=3, ....>
+#   ]
+# }
+#
+# @param users [Array<DTO::User>] list of users
+# @param companies [Hash<Integer, DTO::Company>]
+# @return [Hash<Integer, DTO::Transaction>]
+class TransactionGenerator
+  # users: Array
+  # companies: Hash
+  def initialize(users:, companies:)
+    @users = users || []
+    @companies = companies || {}
+  end
+
+  def perform
+    process
+  end
+
+  private
+
+  def process
+    @users.each_with_object({}) do |user, transactions_by_company|
+      company = @companies[user.company_id]
+      unless company
+        ## TODO: create standard result object
+        puts "Company does not exist #{user.company_id}"
+        next
+      end
+      processed_transactions = transactions_by_company.fetch(company.id, [])
+      transactions_by_company[company.id] = processed_transactions << create_transaction(user, company)
+    end
+  end
+
+  def create_transaction(user, company)
+    DTO::Transaction.new(
+      company_id: company.id,
+      user_id: user.id,
+      previous_balance: user.tokens,
+      top_up: company.top_up,
+      processed_at: Time.now
+    )
+  end
+end
---
+++ spec/transaction_generator_spec.rb
@@ -0,0 +1,81 @@
+require_relative "../lib/transaction_generator"
+require_relative "../lib/dto/user"
+require_relative "../lib/dto/company"
+
+RSpec.describe TransactionGenerator do
+  let(:users) { [] }
+  let(:companies) { {} }
+
+  describe "#perform" do
+    subject(:transaction_generator) { described_class.new(users: users, companies: companies) }
+
+    context "when users and companies are nil" do
+      let(:users) { nil }
+      let(:companies) { nil }
+
+      it "does not create any transactions" do
+        expect { transaction_generator.perform }.not_to output.to_stdout
+      end
+    end
+
+    context "when there are no users and companies" do
+      it "does not create any transactions" do
+        expect { transaction_generator.perform }.not_to output.to_stdout
+      end
+    end
+
+    context "when there are no matching companies for users" do
+      let(:users) do
+        [
+          DTO::User.new(id: 1, first_name: "John", last_name: "Doe", email: "john.doe@example.com", company_id: 1, email_status: true, active_status: true, tokens: 100),
+          DTO::User.new(id: 2, first_name: "Jane", last_name: "Doe", email: "jane.doe@example.com", company_id: 2, email_status: true, active_status: true, tokens: 50)
+        ]
+      end
+
+      let(:companies) do
+        {
+          1 => DTO::Company.new(id: 1, name: "Company A", top_up: 50)
+        }
+      end
+
+      it "does not create any transactions for the users without matching companies" do
+        expect { transaction_generator.perform }.to output("Company does not exist 2\n").to_stdout
+      end
+
+      it "creates transactions only for the users with matching companies" do
+        expect(transaction_generator.perform).to match(
+          1 => [
+            an_instance_of(DTO::Transaction)
+          ]
+        )
+      end
+    end
+
+    context "when all users have matching companies" do
+      let(:users) do
+        [
+          DTO::User.new(id: 1, first_name: "John", last_name: "Doe", email: "john.doe@example.com", company_id: 1, email_status: true, active_status: true, tokens: 100),
+          DTO::User.new(id: 2, first_name: "Jane", last_name: "Doe", email: "jane.doe@example.com", company_id: 2, email_status: true, active_status: true, tokens: 50)
+        ]
+      end
+
+      let(:companies) do
+        {
+          1 => DTO::Company.new(id: 1, name: "Company A", top_up: 50),
+          2 => DTO::Company.new(id: 2, name: "Company B", top_up: 20)
+        }
+      end
+
+      it "creates transactions for all users with matching companies" do
+        expect(transaction_generator.perform).to match(
+          1 => [
+            an_instance_of(DTO::Transaction)
+          ],
+          2 => [
+            an_instance_of(DTO::Transaction)
+          ]
+        )
+      end
+    end
+  end
+end
